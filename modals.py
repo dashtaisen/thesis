551e4e0f910953277cb600a778aca2cc13f6e156
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2018-04-03 20:51:11 -0400

    Fix filename and unicode weirdness

--- modals.py
+++ modals.py
@@ -1,6 +1,8 @@
+"""Tools for analyzing AMR modals"""
 #from __future__ import print_function
 # -*- coding: utf-8 -*-
 import sys
+import os
 import codecs
 sys.path.insert(0, '../amrz/')
 sys.path.insert(0, '../amrz/evaluation/smatch/')
@@ -10,21 +12,21 @@ from amr import AMR
 from smatch import get_amr_line
 import random
 
-GOLD_MODALS = './gold_modal_trees.txt'
-DEV_MODALS = './dev_modal_trees.txt'
+AMR_DATA_DIR = os.path.join(os.pardir,'amrz','data')
+RESULTS_DIR = os.path.join(os.pardir,'results')
+#path to training AMRs
+GOLD_AMRS = os.path.join(AMR_DATA_DIR, 'amr_zh_10k.txt.amr')
+print(GOLD_AMRS)
+#path to dev AMRs
+DEV_AMRS = os.path.join(AMR_DATA_DIR, 'amr_zh_all.txt.dev.amr.basic_abt_feat.1.parsed')
+#path to testing AMRs
+
+GOLD_MODALS = os.path.join(os.curdir, 'gold_modal_trees.txt')
+DEV_MODALS = os.path.join(os.curdir, 'dev_modal_trees.txt')
 
 #path to training/testing unsegmented sents
 #UNSEG_SENTS = '../amrz/data/amr_zh_10k.txt'
-UNSEG_SENTS = '../amrz/data/amr_zh_all.txt'
-
-#path to training/testing segmented sents
-
-#path to training AMRs
-GOLD_AMRS = '../amrz/data/amr_zh_10k.txt.amr'
-
-#path to dev AMRs
-DEV_AMRS = '../amrz/data/amr_zh_all.txt.dev.amr.basic_abt_feat.1.parsed'
-#path to testing AMRs
+UNSEG_SENTS = os.path.join(AMR_DATA_DIR, 'amr_zh_all.txt')
 
 MODAL_ZH = ['能','可能','可以','得起','得了','必须','应该',
         '不得不','会','要','得出来','得起来','得出','该','需','得','可','想','想要','要求']
@@ -37,13 +39,91 @@ MODAL_ZH = ['能','可能','可以','得起','得了','必须','应该',
 # ::wid x1_最近 x2_， x3_我们 x4_通过 x5_知情 x6_人士 x7_从 x8_衡阳市 x9_殡葬 x10_管理处 x11_财务 x12_部门 x13_复印 x14_出 x15_部分 x16_原始 x17_发票 x18_凭证 x19_11 x20_份 x21_（ x22_共计 x23_有 x24_30余 x25_份 x26_） x27_。 x28_
 """
 
-def sample_tree():
-    gold_tuples = read_amrz(GOLD_MODALS)
-    sample_amr = random.choice(gold_tuples[1]).replace('/','')
-    t = Tree.fromstring(sample_amr)
-    t.draw()
+def get_possible_amrs(all_amr_file=None):
+    """Get the IDs of all AMRs with 'possible' concept
+    Inputs:
+        amr_file: file with all the AMRs
+    Returns:
+        list of (id, snt, amr) tuples
+    """
+    if all_amr_file is None:
+        all_amr_file = GOLD_AMRS
+    possible_ids = list() #(id,snt)
+    comments_and_amrs = read_amrz(all_amr_file) #(comment_list, amr_list)
+    comments = comments_and_amrs[0] #{'snt','id'}
+    amrs = comments_and_amrs[1]
+    for i in range(len(amrs)):
+        amr_graph = AMR.parse_AMR_line(amrs[i])
+        node_values = amr_graph.node_values
+        if 'possible' in node_values:
+            possible_ids.append((comments[i]['id'],comments[i]['snt'],amrs[i]))
+            #possible_ids.append((comments[i]['id'].encode('utf8'),comments[i]['snt'].encode('utf8'),amrs[i].encode('utf8')))
+    print("Total number of AMRs with 'possible': {}".format(len(possible_ids)))
+    return sorted(possible_ids,key=lambda x: int(x[0].split(' ')[0].split('.')[1])) #sort by id number
+
+#def write_possible_amrs(destfile,all_amr_file=None):
+def write_possible_amrs(amr_list,destfile):
+    """Write all trees with 'possible' as a concept"""
+    #if all_amr_file is None:
+        #all_amr_file = GOLD_AMRS
+    #possible_ids = get_possible_amrs(all_amr_file)
+    with open(destfile,'w') as dest:
+        for ids,snts,trees in amr_list:
+            dest.write('# ::id {}\n'.format(ids))
+            dest.write('# ::snt {}\n'.format(snts))
+            #tree_string = trees.decode('utf8')
+            tree_string = trees
+            tree_string = Tree.fromstring(tree_string).pformat()
+            #dest.write('{}\n\n'.format(tree_string.encode('utf8')))
+            dest.write('{}\n\n'.format(tree_string))
+
+def get_possible_devs(all_amr_file=None, dev_amr_file=None):
+    """Get sents in the dev set that *should* have 'possible' concept
+    Inputs:
+        all_amr_file: filename with all amrs
+        dev_amr_file: filename with dev amrs
+    Returns:
+        possible_devs: list of (id, snt, amr) tuples
+    """
+    if all_amr_file is None:
+        all_amr_file = GOLD_AMRS
+    if dev_amr_file is None:
+        dev_amr_file = DEV_AMRS
+    possible_ids = [item[0] for item in get_possible_amrs(GOLD_AMRS)]
+    possible_devs = list()
+    dev_tuples = read_amrz(dev_amr_file)
+    dev_comments = dev_tuples[0]
+    dev_amrs = dev_tuples[1]
+    for i in range(len(dev_comments)):
+        if dev_comments[i]['id'] in possible_ids:
+            #possible_devs.append({'id':dev_comments[i]['id'],'snt':dev_comments[i]['snt'],'amr':dev_amrs[i]})
+            possible_devs.append((dev_comments[i]['id'],dev_comments[i]['snt'],dev_amrs[i]))
+    #possible_devs = sorted(possible_devs, key=lambda x:int(x['id'].split(' ')[0].split('.')[1]))
+    possible_devs = sorted(possible_devs, key=lambda x: int(x[0].split(' ')[0].split('.')[1]))
+    print("Total number of sentences in dev/test sets with 'possible': {}".format(len(possible_devs)))
+    return possible_devs
+
+def write_possible_amrs_from_dict(possible_amrs, destfile):
+    """Write AMRs with 'possible' to file
+    Inputs:
+        possible_amrs: list of (id, snt, amr) tuples
+        destfile: file to write to
+    Returns:
+        None (writes to file)
+    """
+    with codecs.open(destfile,'w') as dest:
+        for possible_amr in possible_amrs:
+            dest.write('# ::id {}\n'.format(possible_amr['id']))
+            dest.write('# ::snt {}\n'.format(possible_amr['snt'].encode('utf-8')))
+            tree_string = possible_amr['amr']
+            tree_string = Tree.fromstring(tree_string).pformat()
+            dest.write('{}\n\n'.format(tree_string.encode('utf8')))
+
 
 def find_mismatch():
+    """Find cases where dev amr should have 'possibilty' but doesn't"""
+    possibility_missing = list()
+    possibility_spurious = list()
     mismatches = list()
     gold_tuples = read_amrz(GOLD_MODALS)
     gold_comments = gold_tuples[0]
@@ -76,48 +156,12 @@ def find_mismatch():
 
     return mismatches
 
-def get_possible_ids():
-    possible_ids = list() #(id,snt)
-    comments_and_amrs = read_amrz(GOLD_AMRS) #(comment_list, amr_list)
-    comments = comments_and_amrs[0] #{'snt','id'}
-    amrs = comments_and_amrs[1]
-    for i in range(len(amrs)):
-        #print(comments[i])
-        amr_graph = AMR.parse_AMR_line(amrs[i])
-        node_values = amr_graph.node_values
-        if 'possible' in node_values:
-            possible_ids.append((comments[i]['id'].encode('utf8'),comments[i]['snt'].encode('utf8'),amrs[i].encode('utf8')))
-            #print("Added {}".format(comments[i]['id']))
-    return sorted(possible_ids,key=lambda x: int(x[0].split(' ')[0].split('.')[1])) #sort by id number
-
-def write_possible_ids():
-    possible_ids = get_possible_ids()
-    with open('gold_trees.txt','w') as dest:
-        for ids,snts,trees in possible_ids:
-            dest.write('# ::id {}\n'.format(ids))
-            dest.write('# ::snt {}\n'.format(snts))
-            tree_string = trees.decode('utf8')
-            tree_string = Tree.fromstring(tree_string).pformat()
-            dest.write('{}\n\n'.format(tree_string.encode('utf8')))
-
-def get_possible_devs():
-    possible_ids = [item[0] for item in get_possible_ids()]
-    possible_devs = list()
-    dev_tuples = read_amrz(DEV_AMRS)
-    dev_comments = dev_tuples[0]
-    dev_amrs = dev_tuples[1]
-    for i in range(len(dev_comments)):
-        if dev_comments[i]['id'] in possible_ids:
-            possible_devs.append({'id':dev_comments[i]['id'],'snt':dev_comments[i]['snt'],'amr':dev_amrs[i]})
-    possible_devs = sorted(possible_devs, key=lambda x:int(x['id'].split(' ')[0].split('.')[1]))
-
-    with codecs.open('possible_dev.txt','w') as dest:
-        for possible_dev in possible_devs:
-            dest.write('# ::id {}\n'.format(possible_dev['id']))
-            dest.write('# ::snt {}\n'.format(possible_dev['snt'].encode('utf-8')))
-            tree_string = possible_dev['amr']
-            tree_string = Tree.fromstring(tree_string).pformat()
-            dest.write('{}\n\n'.format(tree_string.encode('utf8')))
+def sample_tree():
+    """Draw a sample tree"""
+    gold_tuples = read_amrz(GOLD_MODALS)
+    sample_amr = random.choice(gold_tuples[1]).replace('/','')
+    t = Tree.fromstring(sample_amr)
+    t.draw()
 
 def get_modal_sents():
     modal_sents = dict()
@@ -139,8 +183,12 @@ def get_modal_sents():
     return modal_sents
 
 if __name__ == "__main__":
-    mismatch = find_mismatch()
-    print(len(mismatch))
+    #possible_amrs = get_possible_amrs(all_amr_file=GOLD_AMRS)
+    #write_possible_amrs(destfile='../results/possible_amrs.txt',all_amr_file=GOLD_AMRS)
+    possible_devs = get_possible_devs(all_amr_file=GOLD_AMRS, dev_amr_file=DEV_AMRS)
+    write_possible_amrs(amr_list=possible_devs,destfile='../results/possible_devs.txt')
+    #mismatch = find_mismatch()
+    #print(len(mismatch))
     #sample_tree()
     #write_possible_ids()
     #get_possible_devs()
